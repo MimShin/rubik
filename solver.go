b6ba227e3635c1c58083eca9f237c58dfecf578f
Mehran Shokouhi <mehran.shokouhi@oracle.com>
2020-02-24 21:47:08 -0500

    Preventing memory exhaustion by limiting number of goroutines and allowing more goroutines at deeper levels - Solves 7 moves in 2 mins, 8 moves in less than 35 mins (4 cores @ 2.5 GHz, 16 GB memory)

--- solver.go
+++ solver.go
@@ -3,15 +3,21 @@ package rubik
 import (
 	"log"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 var wg = sync.WaitGroup{}
 var solved bool
+var gorCnt int32
+
+const gorMax = 1_200_000
 
 // Solve the cube.
 // experiment1 - pruning undo move -> 6 moves: 12 secs, 7 moves 5 mins
 // experiment2 - pruning 3 moves of the same type -> 6 moves: 10 secs, 7 moves 3:30 mins
 // experiment3 - using bitmap for moves -> 6  moves: 9 secs, 7 moves: 3-7 mins
+// experiment4 - Preventing memory exhaustion by limiting number of goroutines -> 7 moves: 2 mins, 8 moves < 35 mins
 func Solve(cube Cube, max int) string {
 	for i := 1; i <= max; i++ {
 		log.Printf("Looking for a solution with %d moves\n", i)
@@ -27,6 +33,8 @@ func Solve(cube Cube, max int) string {
 
 func solve(cube Cube, movesStr string, lastLastM, lastM, max int) {
 	defer wg.Done()
+	defer atomic.AddInt32(&gorCnt, -1)
+
 	if solved {
 		return
 	}
@@ -56,11 +64,17 @@ func solve(cube Cube, movesStr string, lastLastM, lastM, max int) {
 			continue
 		}
 
+		// more goroutines at deeper levels to free memory
+		for gorCnt > gorMax>>max {
+			time.Sleep(1000 * time.Millisecond)
+		}
+
 		newCube := cube
 		move := newCube.MoveX(m)
 		i++
 
 		wg.Add(1)
+		atomic.AddInt32(&gorCnt, 1)
 		go solve(newCube, movesStr+move, lastM, m, max-1)
 	}
 }
